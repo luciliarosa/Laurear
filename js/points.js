4ecb53f0bffb3528b06039d6c034ff4b9c65d53d
victoria <vict.avelino@gmail.com>
2024-04-14 12:51:01 -0300

    Icone_Points
    Ajuste do icone dos points localizado no canto superior direito da pagina.

--- js/points.js
+++ js/points.js
@@ -1,9 +1,12 @@
 // Função para mostrar/ocultar os pontos
 function togglePoints() {
-    let points = document.querySelector('.points-value');
-    if (points.textContent === '****') {
-      points.textContent = '1000'; // Altere aqui para a quantidade real de pontos
-    } else {
-      points.textContent = '****';
-    }
+  let points = document.querySelector('.points-value');
+  let eyeIcon = document.getElementById('eyeIcon');
+  if (points.textContent === '****') {
+    points.textContent = '1000'; // Altere aqui para a quantidade real de pontos
+    eyeIcon.className = 'bi bi-eye'; // Ícone do olho aberto
+  } else {
+    points.textContent = '****';
+    eyeIcon.className = 'bi bi-eye-slash'; // Ícone do olho fechado
   }
+}
